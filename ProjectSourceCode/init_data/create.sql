4a35d3c0f968512397a43b1eff792075f589504c
Lamperr <nla4@proton.me>
2024-04-04 14:22:32 -0600

    Created sql tables for students,tutors,subject and review

--- ProjectSourceCode/init_data/create.sql
+++ ProjectSourceCode/init_data/create.sql
@@ -1,11 +1,38 @@
 DROP TABLE IF EXISTS students;
 CREATE TABLE students (
-    username VARCHAR(50) PRIMARY KEY,
-    password CHAR(60) NOT NULL
+    id SERIAL PRIMARY KEY NOT NULL,
+    subject_id INT NOT NULL,
+    review_id INT NOT NULL,
+    username VARCHAR(50) NOT NULL,
+    first_name VARCHAR(40) NOT NULL,
+    last_name VARCHAR(40) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(200) NOT NULL,
+    year INTEGER NOT NULL,
+    major VARCHAR(30) NOT NULL
 );
 
 DROP TABLE IF EXISTS tutors;
 CREATE TABLE tutors (
-    username VARCHAR(50) PRIMARY KEY,
-    password CHAR(60) NOT NULL
+    id SERIAL PRIMARY KEY NOT NULL,
+    subject_id INT NOT NULL,
+    review_id INT NOT NULL,
+    username VARCHAR(50) NOT NULL,
+    first_name VARCHAR(40) NOT NULL,
+    last_name VARCHAR(40) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(200) NOT NULL
+);
+
+DROP TABLE IF EXISTS subject;
+CREATE TABLE subject (
+    id SERIAL PRIMARY KEY NOT NULL,
+    name VARCHAR(30)
+);
+
+DROP TABLE IF EXISTS review;
+CREATE TABLE review (
+    id SERIAL PRIMARY KEY NOT NULL,
+    rating SMALLINT NOT NULL,
+    summary VARCHAR(500)
 );
