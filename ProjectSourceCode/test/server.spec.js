eb7f43828aee5b13549c3515a81e71045e30f796
cako2445 <cako2445@colorado.edu>
2024-04-07 20:04:01 -0600

    Update register test cases

--- ProjectSourceCode/test/server.spec.js
+++ ProjectSourceCode/test/server.spec.js
@@ -1,81 +1,66 @@
-// // ********************** Initialize server **********************************
+// ********************** Initialize server **********************************
 
-// const server = require('../index.js'); //TODO: Make sure the path to your index.js is correctly added
+const server = require('../index.js'); //TODO: Make sure the path to your index.js is correctly added
 
-// // ********************** Import Libraries ***********************************
+// ********************** Import Libraries ***********************************
 
-// const chai = require('chai'); // Chai HTTP provides an interface for live integration testing of the API's.
-// const chaiHttp = require('chai-http');
-// chai.should();
-// chai.use(chaiHttp);
-// const { assert, expect } = chai;
+const chai = require('chai'); // Chai HTTP provides an interface for live integration testing of the API's.
+const chaiHttp = require('chai-http');
+chai.should();
+chai.use(chaiHttp);
+const { assert, expect } = chai;
 
-// // ********************** DEFAULT WELCOME TESTCASE ****************************
+// ********************** DEFAULT WELCOME TESTCASE ****************************
 
-// describe('Server!', () => {
-//     // Sample test case given to test / endpoint.
-//     it('Returns the default welcome message', done => {
-//         chai
-//             .request(server)
-//             .get('/welcome')
-//             .end((err, res) => {
-//                 expect(res).to.have.status(200);
-//                 expect(res.body.status).to.equals('success');
-//                 assert.strictEqual(res.body.message, 'Welcome!');
-//                 done();
-//             });
-//     });
-// });
+describe('Server!', () => {
+    // Sample test case given to test / endpoint.
+    it('Returns the default welcome message', done => {
+        chai
+            .request(server)
+            .get('/welcome')
+            .end((err, res) => {
+                expect(res).to.have.status(200);
+                expect(res.body.status).to.equals('success');
+                assert.strictEqual(res.body.message, 'Welcome!');
+                done();
+            });
+    });
+});
 
-// // *********************** TODO: WRITE 2 UNIT TESTCASES **************************
-// describe('Testing Register API', () => {
-//     it('positive : /register', done => {
-//         chai
-//             .request(server)
-//             .post('/register')
-//             .send({
-//                 subject_id: 1,
-//                 review_id: 1,
-//                 username: 'johndoe',
-//                 first_name: 'John',
-//                 last_name: 'Doe',
-//                 email: 'johndoe@colorado.edu',
-//                 password: 'password123',
-//                 year: 2,
-//                 major: 'Computer Science'
-//             })
-//             .end((err, res) => {
-//                 // expect(res).to.have.status(200); - needs to be worked on
-//                 // expect(res).to.redirectTo('/login'); - needs to be worked on
-//                 expect(res.body.message).to.equals('Registered!');
-//                 done();
-//             });
-//     });
-// });
+// *********************** TODO: WRITE 2 UNIT TESTCASES **************************
+describe('Testing Register API', () => {
+    it('positive : /register. For tutor', done => {
+        chai
+            .request(server)
+            .post('/register')
+            .redirects(0)
+            .send({
+                username: 'johndoe',
+                password: 'password123',
+                tutor_student_rad: 'tutor'
+            })
+            .end((err, res) => {
+                res.should.have.status(302);
+                res.should.redirectTo('/loginTutor');
+                done();
+            });
+    });
+});
 
-// describe('Testing Register API', () => {
-//     it('negative : /register. Checking duplicate username', done => {
-//         chai
-//             .request(server)
-//             .post('/register')
-//             .send({
-//                 subject_id: 1,
-//                 review_id: 1,
-//                 username: 'johndoe',
-//                 first_name: 'John',
-//                 last_name: 'Doe',
-//                 email: 'johndoe@colorado.edu',
-//                 password: 'password123',
-//                 year: 2,
-//                 major: 'Computer Science'
-//             })
-//             .end((err, res) => {
-//                 // expect(res).to.have.status(400); - needs to be worked on
-//                 // expect(res).to.redirectTo('/register'); - needs to be worked on
-//                 expect(res.body.message).to.equals('Invalid registration!');
-//                 done();
-//             });
-//     });
-// });
-
-// ********************************************************************************
+describe('Testing Register API', () => {
+    it('negative : /register. No username.', done => {
+        chai
+            .request(server)
+            .post('/register')
+            .redirects(0)
+            .send({
+                password: 'password123',
+                tutor_student_rad: 'tutor'
+            })
+            .end((err, res) => {
+                res.should.have.status(400);
+                res.text.should.equal('Missing required field');
+                done();
+            });
+    });
+});
