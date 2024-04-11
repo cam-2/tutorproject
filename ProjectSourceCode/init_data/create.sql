53bff6d1084877bb10e77c2984c70f81fbbe4235
BassettC <caba9156@colorado.edu>
2024-04-11 00:00:18 -0600

    Modified SQL

--- ProjectSourceCode/init_data/create.sql
+++ ProjectSourceCode/init_data/create.sql
@@ -21,7 +21,8 @@ CREATE TABLE tutors (
     first_name VARCHAR(40),
     last_name VARCHAR(40),
     email VARCHAR(100),
-    password VARCHAR(200) NOT NULL
+    password VARCHAR(200) NOT NULL/*,
+    fk_avail_id INT REFERENCES availability(id)*/
 );
 
 DROP TABLE IF EXISTS subject;
@@ -34,5 +35,34 @@ DROP TABLE IF EXISTS review;
 CREATE TABLE review (
     id SERIAL PRIMARY KEY NOT NULL,
     rating SMALLINT NOT NULL,
-    summary VARCHAR(500)
+    summary VARCHAR(500),
+    fk_tutor_id INT REFERENCES tutors(id) NOT NULL,
+    fk_student_id INT REFERENCES students(id) NOT NULL,
+    fk_subject_id INT REFERENCES subject(id) NOT NULL,
+    post_date TIMESTAMP
+);
+
+DROP TABLE IF EXISTS timeslot;
+CREATE TABLE timeslot (
+    id SERIAL PRIMARY KEY NOT NULL,
+    /*fk_avail_id INT REFERENCES availability(id),*/
+    fk_tutor_id INT REFERENCES tutors(id) NOT NULL,
+    fk_subject_id INT REFERENCES subject(id) NOT NULL, 
+    start_time TIMESTAMP,
+    end_time TIMESTAMP
+);
+
+/*
+DROP TABLE IF EXISTS availability;
+CREATE TABLE availability (
+    id SERIAL PRIMARY KEY NOT NULL,
+    fk_tutor_id INT REFERENCES tutors(id) 
+);
+*/
+
+DROP TABLE IF EXISTS tutor_to_student;
+CREATE TABLE tutor_to_student (
+    id SERIAL PRIMARY KEY NOT NULL,
+    fk_tutor_id INT REFERENCES tutors(id) NOT NULL,
+    fk_student_id INT REFERENCES students(id) NOT NULL
 );
