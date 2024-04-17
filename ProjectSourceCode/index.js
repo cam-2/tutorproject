988a2c3f66f7e7c20e1d243c67c9d3fa167ac9d7
Cameron <143983164+cam-2@users.noreply.github.com>
2024-04-17 11:46:09 -0600

    Adding broaddbConfig function for deploying to Web Services

--- ProjectSourceCode/index.js
+++ ProjectSourceCode/index.js
@@ -27,7 +27,15 @@ const dbConfig = {
   password: process.env.POSTGRES_PASSWORD, // the password of the user account
 };
 
-const db = pgp(dbConfig);
+const broaddbConfig = {
+  host: process.env.host, // the database server
+  port: 5432, // the database port
+  database: process.env.POSTGRES_DB, // the database name
+  user: process.env.POSTGRES_USER, // the user account to connect with
+  password: process.env.POSTGRES_PASSWORD, // the password of the user account
+};
+
+const db = pgp(broaddbConfig);
 // test your database
 db.connect()
   .then(obj => {
